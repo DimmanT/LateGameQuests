63ea3f666933c3cc468c693f8d155985e7a35b66
DimmanT <DmitryDev33@yandex.ru>
2026-07-12 22:30:49 +0300

    start changing quest giver mechanism

---
+++ Events/IncidentWorker_QuestGiver.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace LoGiQ.Events
+{
+    public class IncidentWorker_QuestGiver : IncidentWorker//IncidentWorker_GiveQuest
+    {
+        static List<QuestScriptDef> LoGiQ_Quests;
+        static float minPointsOverAll = 0;
+
+        public override float BaseChanceThisGame
+        {
+            get
+            {
+                float[] MULT = { 0, 0.2f, 0.5f, 1, 2, 4 };
+                var chance = base.BaseChanceThisGame;
+                var rate = Math.Min(5, Mod.Settings.ChoosenRate);
+                    rate = Math.Max(0, rate);
+
+                Log.Message($"Chance is {chance}->{chance * MULT[rate]}");
+                return chance * MULT[rate];
+            }
+        }
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            if (!base.CanFireNowSub(parms))
+            {
+                return false;
+            }
+
+            if (LoGiQ_Quests.NullOrEmpty())
+                RecacheLoGiQQuests();
+
+            Log.Message($"CanFireNowSub points {parms.points}/{minPointsOverAll}");
+            if (parms.points < minPointsOverAll)
+                return false;
+
+            var ok =  PawnsFinder.AllMapsCaravansAndTravellingTransporters_Alive_FreeColonists_NoSuspended.Any();
+
+            Log.Message($"CanFireNowSub {ok}");
+
+            return ok;
+        }
+
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            Log.Message($"EXECUTE {parms.points}.");
+            QuestScriptDef questScriptDef = ChooseLoGiQRandomQuest(parms.points, parms.target);
+            if (questScriptDef == null)
+            {
+                Log.Message("No quest to run now.");
+                return false;
+            }
+            Log.Message($"quest is {questScriptDef.defName}");
+            parms.questScriptDef = questScriptDef;
+            GiveQuest(parms, questScriptDef);
+            return true;
+        }
+
+        protected virtual void GiveQuest(IncidentParms parms, QuestScriptDef questDef)
+        {
+            Quest quest = QuestUtility.GenerateQuestAndMakeAvailable(questDef, parms.points);
+            if (!quest.hidden && questDef.sendAvailableLetter)
+            {
+                QuestUtility.SendLetterQuestAvailable(quest);
+            }
+        }
+
+        static protected bool CanQuestOccurOnTile(PlanetTile tile, QuestScriptDef quest)
+        {
+            if (!tile.Valid)
+            {
+                return true;
+            }
+            if (quest != null)
+            {
+                PlanetLayerDef layerDef = tile.LayerDef;
+                if (!quest.layerWhitelist.NullOrEmpty() && !quest.layerWhitelist.Contains(layerDef))
+                {
+                    return false;
+                }
+                if (!quest.layerBlacklist.NullOrEmpty() && quest.layerBlacklist.Contains(layerDef))
+                {
+                    return false;
+                }
+                if (!quest.canOccurOnAllPlanetLayers && layerDef.onlyAllowWhitelistedIncidents && (quest.layerWhitelist.NullOrEmpty() || !quest.layerWhitelist.Contains(layerDef)))
+                {
+                    return false;
+                }
+            }
+            return !tile.LayerDef.onlyAllowWhitelistedQuests;
+        }
+
+        static void RecacheLoGiQQuests()
+        {
+            LoGiQ_Quests = new List<QuestScriptDef>();
+            var collection = DefDatabase<QuestScriptDef>.AllDefs.Where((QuestScriptDef x) => x.defName.StartsWith("LoGiQ"));
+            LoGiQ_Quests.AddRange(collection);
+
+            //... find minimal required points ...
+            minPointsOverAll = 9999;
+            foreach (var q in collection)
+                if (q.rootMinPoints < minPointsOverAll)
+                    minPointsOverAll = q.rootMinPoints;
+
+            Log.Message($"Recaching LoGiQ quests. Found {LoGiQ_Quests.Count()} quests. MinPointsOverAll = {minPointsOverAll}.");
+        }
+
+        protected static QuestScriptDef ChooseLoGiQRandomQuest(float points, IIncidentTarget target)
+        {
+            if(LoGiQ_Quests.NullOrEmpty())
+                RecacheLoGiQQuests();
+            QuestScriptDef result = null;
+            LoGiQ_Quests.Where((QuestScriptDef x) => x.CanRun(points, target) && CanQuestOccurOnTile(target.Tile, x))
+                        .TryRandomElementByWeight((QuestScriptDef x) => RimWorld.NaturalRandomQuestChooser.GetNaturalRandomSelectionWeight(x, points, target.StoryState), out result);
+
+            return result;
+        }
+    }
+}
---
+++ Misc/ModSettings.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace LoGiQ
+{
+    public class ModSettings : Verse.ModSettings
+    {
+        private Vector2 buttonPosition = Vector2.zero;
+        private float viewHeight = 0;
+        public int ChoosenRate { get; private set; } = 3;
+        private string ChoosenRateStr;
+        public void DoSettingsWindowContents(Rect inRect)
+        {
+            var NAMES = new List<string> { "Never".Translate(), "Very rarely".Translate(), "Rarely".Translate(), "Normal".Translate(), "Often".Translate(), "Very often".Translate() };
+            Rect viewRect = new Rect(0,0,inRect.width - 16, viewHeight);
+            Widgets.BeginScrollView(inRect, ref buttonPosition, viewRect);
+
+            Listing_Standard listing = new Listing_Standard();
+            listing.ColumnWidth = inRect.width;
+            Rect listingRect = inRect.AtZero();
+            listingRect.height = 1024;
+            listing.Begin(listingRect);
+            listing.Label("LoGiQSettings_RateButtonText".Translate());
+            if (ChoosenRateStr.NullOrEmpty())
+                ChoosenRateStr = NAMES[ChoosenRate];
+            if (listing.ButtonText(ChoosenRateStr))
+            {
+                var rates = new List<FloatMenuOption>();
+                rates.Add(new FloatMenuOption(NAMES[0], () => { ChoosenRate = 0; ChoosenRateStr = NAMES[ChoosenRate]; Log.Message(ChoosenRateStr);}));
+                rates.Add(new FloatMenuOption(NAMES[1], () => { ChoosenRate = 1; ChoosenRateStr = NAMES[ChoosenRate]; Log.Message(ChoosenRateStr);}));
+                rates.Add(new FloatMenuOption(NAMES[2], () => { ChoosenRate = 2; ChoosenRateStr = NAMES[ChoosenRate]; Log.Message(ChoosenRateStr);}));
+                rates.Add(new FloatMenuOption(NAMES[3], () => { ChoosenRate = 3; ChoosenRateStr = NAMES[ChoosenRate]; Log.Message(ChoosenRateStr);}));
+                rates.Add(new FloatMenuOption(NAMES[4], () => { ChoosenRate = 4; ChoosenRateStr = NAMES[ChoosenRate]; Log.Message(ChoosenRateStr);}));
+                rates.Add(new FloatMenuOption(NAMES[5], () => { ChoosenRate = 5; ChoosenRateStr = NAMES[ChoosenRate]; Log.Message(ChoosenRateStr); }));
+                Find.WindowStack.Add(new FloatMenu(rates));
+            }
+            listing.End();
+
+            viewHeight = listing.CurHeight;
+            Widgets.EndScrollView();
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            var tmp = ChoosenRate;
+            Scribe_Values.Look(ref tmp, "ChoosenRate",3);
+            ChoosenRate = tmp;
+        }
+
+    }
+    public class Mod : Verse.Mod
+    {
+        public static ModSettings Settings {get; private set;}
+        public Mod(ModContentPack content) : base(content)
+        {
+            Settings = GetSettings<ModSettings>();
+        }
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Settings.DoSettingsWindowContents(inRect);
+        }
+        public override string SettingsCategory()
+        {
+            return "Late Game Quests";
+        }
+    }
+
+}
